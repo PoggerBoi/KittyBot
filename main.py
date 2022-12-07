ce10b5d171502997ee9cf3d08d02aa3f8fb5f864
PoggerBoi <Zellolarsh@gmail.com>
2022-12-07 22:22:17 +0100

    Added rate limiting by token amount spent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import openai
 import logging
+import threading
 
 logging.basicConfig(filename='bot.log', encoding='utf-8', level=logging.CRITICAL)
 with open('keys.txt', 'r') as file:
@@ -9,6 +10,11 @@ with open('keys.txt', 'r') as file:
 
 
 class MyClient(discord.Client):
+    balance = 2000
+
+    def increase_balance(self, inc: int):
+        self.balance += inc
+
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -31,26 +37,45 @@ class MyClient(discord.Client):
         content = message.content.split(' ', 1)[1]
         logging.critical("sender: " + message.author.name)
         logging.critical("message: " + content)
+        logging.critical("balance: " + str(self.balance))
 
         if not message.channel.permissions_for(message.mentions[ind]).send_messages:
             print("No permissions to answer a ping")
             return
 
-        if "image" in content.lower():
+        if "!balance" in content.lower():
+            await message.channel.send("Current token balance: " + str(self.balance))
+        elif "image" in content.lower():
+            if self.balance < 1000:
+                await message.channel.send("Token balance insufficient. Try again later")
+                return
+            self.balance -= 1000
             response = openai.Image.create(prompt=content, n=1, size="1024x1024")
             logging.critical("response time: " + str(response.response_ms))
             logging.critical(response)
             await message.channel.send(response['data'][0]['url'])
         else:
+            if self.balance < 140:
+                await message.channel.send("Token balance insufficient. Try again later")
+                return
             # cheap - text-curie-001
             # capable - text-davinci-003
             response = openai.Completion.create(model="text-curie-001", prompt=content, temperature=0.6,
                                                 max_tokens=140, presence_penalty=1, frequency_penalty=1)
             logging.critical("response time: " + str(response.response_ms))
             logging.critical(response)
+            self.balance -= response['usage']['total_tokens']
             await message.channel.send(response["choices"][0]["text"].replace('\n\n', '\n'))
 
 
 client = MyClient()
+
+
+def timer_loop():
+    client.increase_balance(10)
+    threading.Timer(5.0, lambda: timer_loop()).start()
+
+
+timer_loop()
 client.run(discKey)
 
